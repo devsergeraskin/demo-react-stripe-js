5ff0698d8beec48f270ee6756c19d579fdaa7332
serge <devsergeraskin@gmail.com>
2020-09-20 22:30:06 -0400

    finnish homework

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import styled from "@emotion/styled";
 import axios from "axios";
 
@@ -6,6 +6,8 @@ import Row from "./prebuilt/Row";
 import BillingDetailsFields from "./prebuilt/BillingDetailsFields";
 import SubmitButton from "./prebuilt/SubmitButton";
 import CheckoutError from "./prebuilt/CheckoutError";
+
+import {CardElement, useStripe, useElements} from "@stripe/react-stripe-js"
 
 const CardElementContainer = styled.div`
   height: 40px;
@@ -18,41 +20,92 @@ const CardElementContainer = styled.div`
   }
 `;
 
-const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
-  const [isProcessing, setProcessingTo] = useState(false);
-  const [checkoutError, setCheckoutError] = useState();
 
-  const handleFormSubmit = async ev => {
-    ev.preventDefault();
+const CheckoutForm = ({price, onSuccessfulCheckout}) => {
+    const [isProcessing, setProcessingTo] = useState(false);
+    const [checkoutError, setCheckoutError] = useState();
 
-    const billingDetails = {
-      name: ev.target.name.value,
-      email: ev.target.email.value,
-      address: {
-        city: ev.target.city.value,
-        line1: ev.target.address.value,
-        state: ev.target.state.value,
-        postal_code: ev.target.zip.value
-      }
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleFormSubmit = async ev => {
+        ev.preventDefault();
+
+        const billingDetails = {
+            name: ev.target.name.value,
+            email: ev.target.email.value,
+            address: {
+                city: ev.target.city.value,
+                line1: ev.target.address.value,
+                state: ev.target.state.value,
+                postal_code: ev.target.zip.value
+            }
+        };
+
+        setProcessingTo(true);
+
+        const {data: clientSecret} = await axios.post('/api/payment_intents', {
+            amount: price * 100
+        });
+
+        const  cardElement = elements.getElement(CardElement);
+
+
+        const paymentMethodReq =  await stripe.createPaymentMethod({
+            type:'card',
+            card:cardElement,
+            billing_details: billingDetails
+        });
+
+        const confirmedCardPayment = await stripe.confirmCardPayment (clientSecret,{
+            payment_method: paymentMethodReq.paymentMethod.id
+        });
+
+        console.log(confirmedCardPayment)
+
+        onSuccessfulCheckout();
+
     };
-  };
 
-  return (
-    <form onSubmit={handleFormSubmit}>
-      <Row>
-        <BillingDetailsFields />
-      </Row>
-      <Row>
-        <CardElementContainer></CardElementContainer>
-      </Row>
-      {checkoutError && <CheckoutError>{checkoutError}</CheckoutError>}
-      <Row>
-        <SubmitButton disabled={isProcessing}>
-          {isProcessing ? "Processing..." : `Pay $${price}`}
-        </SubmitButton>
-      </Row>
-    </form>
-  );
+
+    //strip.com/docs/js
+    const CardElementOption = {
+        style: {
+            base: {
+                fontSize: '16px',
+                color: "#fff",
+                "::placeholder": {
+                    color: '#87bbfd'
+                }
+            },
+            invalid: {
+                color: "#FFC7EE",
+                iconColor: "#FFC7EE"
+            },
+            complite: {}
+        },
+        hidePostalCode: true
+    };
+
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <Row>
+                <BillingDetailsFields/>
+            </Row>
+            <Row>
+                <CardElementContainer>
+                    <CardElement options={CardElementOption}></CardElement>
+                </CardElementContainer>
+            </Row>
+            {checkoutError && <CheckoutError>{checkoutError}</CheckoutError>}
+            <Row>
+                <SubmitButton disabled={isProcessing}>
+                    {isProcessing ? "Processing..." : `Pay $${price}`}
+                </SubmitButton>
+            </Row>
+        </form>
+    );
 };
 
 export default CheckoutForm;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,16 +2,24 @@ import Head from "next/head";
 import styled from "@emotion/styled";
 import GlobalStyles from "./prebuilt/GlobalStyles";
 
+import {loadStripe} from "@stripe/stripe-js"
+import {Elements} from "@stripe/react-stripe-js"
+
+const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
+
+// don't call loadStripe
+// don't want to load more than you have to
+
 const Layout = ({ children, title }) => {
   return (
     <>
-      <GlobalStyles />
+        <GlobalStyles />
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {children}
+      <Elements stripe={stripePromise}>{children}</Elements>
     </>
   );
 };
